8747942856d3310525d44d099a083c68c10d5d0c
Thrivikrama rao Kavuri <129424538+VikramKavuri@users.noreply.github.com>
2025-08-11 21:02:35 -0400

    Update data.js

--- frontend/src/mock/data.js
+++ frontend/src/mock/data.js
@@ -134,7 +134,7 @@ export const mockContactInfo = {
   email: "thrivikr@buffalo.edu",
   phone: "(716) 253-5373",
   location: "Buffalo, New York, US",
-  linkedIn: "https://linkedin.com/in/thrivikramarao-kavuri-7290b6147",
+  linkedIn: "https://www.linkedin.com/in/thrivikrama-rao-kavuri-7290b6147/",
   github: "https://github.com/VikramKavuri",
   availability: "Open to new opportunities",
   timezone: "EST (Eastern Standard Time)"
