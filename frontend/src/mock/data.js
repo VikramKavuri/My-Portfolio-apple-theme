accfc26da5a458d613b3b8fd594b8f0b03d7a9de
Thrivikrama rao Kavuri <129424538+VikramKavuri@users.noreply.github.com>
2025-08-13 13:51:22 -0400

    Update data.js

--- frontend/src/mock/data.js
+++ frontend/src/mock/data.js
@@ -56,13 +56,13 @@ export const mockSkills = {
           name: "Python",
           level: 95,
           iconType: "image",
-          iconUrl: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"
+          iconUrl: "https://www.citypng.com/public/uploads/preview/hd-python-logo-symbol-transparent-png-735811696257415dbkifcuokn.png?v=2025061923"
         },
         {
           name: "SQL",
           level: 90,
           iconType: "image",
-          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"
+          icon: "https://img.favpng.com/6/22/18/clip-art-microsoft-azure-sql-database-microsoft-sql-server-png-favpng-vuy3jBrd8YXkMut1vRTrG8B3F.jpg"
         },
         {
           name: "PySpark",
